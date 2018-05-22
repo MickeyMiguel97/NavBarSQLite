fc04af3dfecabce1da235ce5b8c49df3d54dff93
MickeyMiguel97 <00145016@uca.edu.sv>
2018-05-21 22:12:07 -0600

    Clase persona agregada

--- app/src/main/java/com/example/yanirayanes/navbar_sqlite/objeto/persona.java
+++ app/src/main/java/com/example/yanirayanes/navbar_sqlite/objeto/persona.java
@@ -1,4 +1,41 @@
 package com.example.yanirayanes.navbar_sqlite.objeto;
 
 public class persona {
+    private String carnet;
+    private String nombre;
+    private int nota;
+
+    public persona(){
+
+    }
+
+    public persona(String carnet, String nombre, int nota){
+        this.carnet = carnet;
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
 }
