23d434ba39035727bf8d5cb0a1747967c31ad306
MickeyMiguel97 <00145016@uca.edu.sv>
2018-05-22 00:51:49 -0600

    Main Activity lista, solo faltan las clases de los fragmentos

--- app/src/main/java/com/example/yanirayanes/navbar_sqlite/MainActivity.java
+++ app/src/main/java/com/example/yanirayanes/navbar_sqlite/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.yanirayanes.navbar_sqlite;
 
+import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,8 +15,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.yanirayanes.navbar_sqlite.fragmentos.agregar_notas;
+import com.example.yanirayanes.navbar_sqlite.fragmentos.agregar_persona;
+import com.example.yanirayanes.navbar_sqlite.fragmentos.lista_notas;
+import com.example.yanirayanes.navbar_sqlite.fragmentos.show_notas;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, agregar_notas.OnFragmentInteractionListener,
+        agregar_persona.OnFragmentInteractionListener, lista_notas.OnFragmentInteractionListener, show_notas.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,24 +86,33 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        android.support.v4.app.Fragment My_Fragment = null;
+        boolean fragmentSelecconado = false;
         int id = item.getItemId();
+        fragmentSelecconado = true;
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            My_Fragment = new agregar_persona();
+            fragmentSelecconado = true;
         } else if (id == R.id.nav_gallery) {
-
+            My_Fragment = new agregar_notas();
+            fragmentSelecconado = true;
         } else if (id == R.id.nav_slideshow) {
+            My_Fragment = new show_notas();
+            fragmentSelecconado = true;
+        }
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if(fragmentSelecconado){
+            getSupportFragmentManager().beginTransaction().replace(R.id.contenido, My_Fragment).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
